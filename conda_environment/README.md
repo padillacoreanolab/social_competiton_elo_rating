7a462fb34a1fda97ddadc88b66168c10138550a8
ryoiwata <ryoiwata@github.com>
2022-12-20 19:02:41 -0500

    doc: instructions on how to install conda environment

---
+++ conda_environment/README.md
@@ -0,0 +1,8 @@
+# Setting up the Conda Environment
+
+## Installing with Windows, Linux, or Mac 
+0. Open your terminal. We recommend Anaconda Prompt or Command Prompt for Windows and Terminal for Mac/Linux
+1. Change to a directory where you want to save your Conda Environment
+    - From any directory of this repository, we recommend doing `cd $(git rev-parse --show-toplevel)` to get to the root directory.
+2. Copy and paste the commands one line at a time from this file into your terminal(all the lines that don't have the `#` in front, which are comments): 
+    - [./environment_install.sh](./environment_install.sh)
---
+++ conda_environment/environment_install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Moving to the root directory of the Github repo
+cd $(git rev-parse --show-toplevel)
+
+# Turning off the Conda Environment 
+# so that you don't add anything onto an existing environment
+conda deactivate
+
+# Creating the Conda environment
+# Doing it in the root directory to simplify
+conda create --prefix ./elo_rating_env python=3.9 --yes
+# Turning on the Conda environment
+conda activate ./elo_rating_env
+
+# Installing general data processing libraries
+conda install -c conda-forge notebook --yes
+conda install -c conda-forge pandas --yes
+conda install -c conda-forge openpyxl --yes
+conda install -c conda-forge matplotlib --yes
+conda install -c conda-forge scipy --yes
+conda install -c conda-forge seaborn --yes
+conda install -c conda-forge opencv --yes
+conda install -c anaconda pylint --yes
